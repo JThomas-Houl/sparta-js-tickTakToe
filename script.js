c1a51f72e78a9dfca059eab9cf5b5197929687cf
Tech-A25 <tech-a25@Tech-A25s-MacBook-Air.local>
2018-02-22 21:34:16 +0000

    i h2 print its x turn

---
+++ script.js
@@ -0,0 +1,52 @@
+/* Tick Tac Toe
+MVP: - let the user click on to the boxes and be able to add a X or O
+	 - be able to win the game: you need 3 in the same row to win
+	 - first step be able to win in a single line i.e. topRow_0_2, middleRow_3_5 bottomRow_6_8
+	 - 
+*/
+
+
+//variables:
+
+var reset = $(".reset");
+var playerTurn = $(".playerTurn");
+var Turns = 9;
+
+
+var turnX = "X";
+var turnO = "O";
+
+var topRow = $(".topRow_0_2");
+var middleRow = $(".middleRow_3_5");
+var bottomRow = $(".bottomRow_6_8");
+
+
+//Functions:
+
+$(function(event){ //checks if the dom is ready can run before the html
+	console.log("DOM is ready"); 
+
+	playerTurn.on("click", function(event){  
+		console.log(playerTurn.html()); //player click event debug print to console.log
+
+		for(var i = 0; i >= 9; i++){ //nedd to add the 
+			console.log(playerTurn + i);
+		} 
+
+
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
